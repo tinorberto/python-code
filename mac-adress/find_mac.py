a78e9848e3b3e1d97275fad0a0602ee169571ecb
Tiago da Costa Norberto <tiago.cnorberto@pbh.gov.br>
2019-04-02 08:43:33 -0300

    Mac

---
+++ mac-adress/find_mac.py
@@ -0,0 +1,14 @@
+filepath = 'D:/repositorio_python/mac-adress/oui.txt'  
+mac_full = "20-47-47"
+mac_oui = mac_full[0:9]
+print (mac_oui)
+print (type(mac_oui))
+with open(filepath,  encoding="utf8") as fp:  
+   line = fp.readline()
+   cnt = 1
+   while line:
+       #print("Line {}: {}".format(cnt, line.strip()))
+       if line.strip().find(mac_oui) is not -1:
+            print("Line {}: {}".format(cnt, line.strip()))
+       line = fp.readline()
+       cnt += 1
